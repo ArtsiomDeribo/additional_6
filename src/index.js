3c035752e57121ba026f729375fdc656e589b9a6
ArtsiomDeribo <31903188+ArtsiomDeribo@users.noreply.github.com>
2017-09-17 01:34:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,49 +20,35 @@ module.exports = function zeros(expression) {
 
         var lol = 0;
         lol = ciferka.join('');
-        
+
         if (counter == 1){
-            for (var n = 1; n <= lol; n++){
-                var kek = n;
-                while (kek % 2 == 0){
-                    dva++;
-                    kek /= 2;
+            while (lol > 1){
+                var looool = lol;
+                while (looool % 2 == 0){
+                    dva++
+                    looool /= 2;
                 }
-                while (kek % 5 == 0){
-                    pyat++;
-                    kek /= 5;
+                while (looool % 5 == 0){
+                    pyat++
+                    looool /= 5;
                 }
-                
-                }
+                lol--;
             }
-                
+        } else if (counter == 2){
+            while (lol > 1){
+                var looool = lol;
+                while (looool % 2 == 0){
+                    dva++
+                    looool /= 2;
+                }
+                while (looool % 5 == 0){
+                    pyat++
+                    looool /= 5;
+                }
+                lol -= 2;
+            }
         }
-            
-        if (counter == 2){
-            if ((lol % 2) == 0){
-                for (var m = 2; m <= lol; m += 2){
-                    var kek1 = m;
-                    while (kek1 % 2 == 0){
-                        dva++;
-                        kek1 /= 2;
-                    }
-                    while (kek1 % 5 == 0){
-                        pyat++;
-                        kek1 /= 5;
-                    }
-                }
-                }
-            } else {
-                for (var l = 1; l <= lol; l += 2){
-                    var kek2
-                    while (kek2 % 5 == 0){
-                        pyat++;
-                        kek2 /= 5;
-                    }
-                    }
-            
-            
-    
+        
 }
 if (pyat < dva){
     return pyat;
